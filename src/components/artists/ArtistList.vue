1f0baf1b5f64340d87049a312672eed1d9d18287
Henry Anyimadu <henry@whatsfabric.com>
2025-02-15 01:47:44 -0600

    feat: updated ArtistList styling and added artist rarity

--- src/components/artists/ArtistList.vue
+++ src/components/artists/ArtistList.vue
@@ -22,23 +22,29 @@ onMounted(() => {
   userPopularity();
 })
 </script>
-
+<!--<h3 class="font-bold text-white text-xl">Your popularity score = {{popularityScore}} </h3>-->
 <template>
-  <div class="space-y-4">
-    <h3 class="font-bold text-white text-xl">Your popularity score = {{popularityScore}} </h3>
+  <h3 class="font-bold text-white text-xl">Your Music Taste Rarity: {{ Math.round(popularityScore) }}%</h3>
+  <br />
+  <h3 class="font-bold text-white mt-8 text-xl"> <strong>Rarity</strong> is the chance that another Spotify user has a
+  similar music taste to you. Artist rarity is the chance that a random Spotify user has listened to that artist recently.</h3>
+
+  <div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-10 m-12 max-w-full">
     <div
         v-for="(artist, index) in artists"
         :key="artist.id"
-        class="flex items-center gap-4 p-4 bg-white rounded-lg shadow hover:shadow-md transition-shadow"
+        class="flex-col items-center h-full gap-12 p-6 bg-[hsl(225,20%,10%)] border-[hsl(225,20%,50%)] rounded-lg shadow animate-fancy-unhover hover:animate-fancy-hover"
     >
-      <span class="text-lg font-medium text-gray-500 w-3 h-8">{{ index + 1 }}</span>
       <img
-          :src="artist.images[1]?.url"
+          :src="artist.images[0]?.url"
           :alt="artist.name"
-          class="w-8 h-8 object-cover rounded-full"
+          class="w-full h-3/4 object-cover pb-4 rounded"
       />
-      <div>
-        <h3 class="font-medium">{{ artist.name }} {{artist.popularity}}</h3>
+      <span class="text-xl text-left font-extrabold text-white pb-2">{{ index + 1 }}</span>
+      <br />
+      <span class="text-lg text-right font-semibold text-[hsl(225,60%,60%)] pb-2">Rarity: {{ artist.popularity }}</span>
+      <div class="text-[hsl(225,20%,90%)] truncate flex-wrap">
+        <h3 class="font-bold text-wrap ">{{ artist.name }}</h3>
         <p class="text-sm text-gray-600">
           {{ artist.genres.slice(0, 3).join(', ') }}
         </p>
